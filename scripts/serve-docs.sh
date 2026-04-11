b4d1ba5847ffd93446635a159bd3af5225fdcc71
Jonathan Hadida <jhadida.pro@gmail.com>
2026-04-11 14:46:30 -0700

    Move bash scripts to scripts/ subfolder

--- scripts/serve-docs.sh
+++ scripts/serve-docs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$REPO_DIR"
 
 echo "==> Installing docs dependencies..."
